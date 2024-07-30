7d81c0198e0c314d95f2b07b17144e040bcc576c
wilyJ80 <abjurandam@gmail.com>
2024-07-30 00:09:23 -0300

    Cleanup, final build, hope it works (last one worked)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  /* css: {
-    postcss: {
-      plugins: [tailwindcss()]
-    }
-  }, */
   base: '/vite-hello-world-daisyui/',
 })
