fd453ed1072004ed34def4b6e110147f721257ee
wilyJ80 <abjurandam@gmail.com>
2024-07-30 00:51:49 -0300

    Add base comments

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // Remove if your app root URL is not a subpath
+  // Change depending on your app root URL subpath
   base: '/vite-hello-world-daisyui/',
 })
