ca3045a487b8b48c551ed9b64543c4f15825a913
wilyJ80 <abjurandam@gmail.com>
2024-07-29 23:45:06 -0300

    Deploy version

--- README.md
+++
@@ -1,1 +0,0 @@
-# vite-hello-world-daisyui
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import daisyui from "daisyui";
-export default {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-}
-
